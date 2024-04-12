e5b3d7414cb2674f97f67b3908dca1f12ad73bf6
sekiro18w <155962553+sekiro18w@users.noreply.github.com>
2024-04-12 19:02:47 +0530

    Time: 37 ms (49.77%) | Memory: 16.7 MB (39.80%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,6 +1,5 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
         i=0
         j=0
         n=len(s)
@@ -11,7 +10,6 @@ class Solution:
                 cur+=s[i]
                 i+=1
             j+=1
-        print(cur)
         return cur==s
             
             
